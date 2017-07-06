6061d97d4fbf174a70aab0b8edaebfdc0747f0b6
herbertgaoo <idaidai@outlook.com>
2017-07-06 17:57:31 +0800

    img type dialog add

--- script/opendialog.js
+++ script/opendialog.js
@@ -14,26 +14,25 @@ function $dialog(param) {
 	if(param.modal && $(".modal_bg").length == 0) {
 		$("body").append("<div class='modal_bg' />")
 	}
-	$("body").append("<div id='"+param.id+"' class='opendialog noselect'/>");
+	$("body").append("<div id='"+param.id+"' data-modal='"+param.modal+"' class='opendialog noselect'/>");
 	$("#"+param.id).append("<div class='opendialog_header'><span class='title'>"
 			+param.title+"</span><span class='closeBtn' onclick=closeOpenDialog('"+param.id+"');>X</span></div>");
 	$("#"+param.id).append("<div class='dialog_content'/>")
-	$("#"+param.id+" .dialog_content").load(param.href);
-	//calc postion and resize
-	var height = document.documentElement.clientHeight;
-	var width = document.documentElement.clientWidth;
-	var offsetTop = (height - param.height - 10) / 2;
-	var offsetLeft = (width - param.width) / 2;
-	$("#"+param.id).css("top",offsetTop);
-	$("#"+param.id).css("left",offsetLeft);
-	$("#"+param.id).css("height",param.height);
-	$("#"+param.id).css("width",param.width);
+	if(param.href != "" && param.href != undefined) {
+		$("#"+param.id+" .dialog_content").load(param.href);
+	}
+	$("#"+param.id+" .dialog_content").append(param.content);
+	
+	if(param.type == "img" ) {
+		imgType(param.id);
+	}
+	calculatePosition(param.id, param.height, param.width);
 	eventListening();
 }
 
 function closeOpenDialog(id) {
 	$("#"+id).remove();
-	if($(".opendialog").length == 0) {
+	if($(".opendialog[data-modal=true]").length == 0) {
 		$(".modal_bg").remove();
 	}
 }
@@ -46,6 +45,27 @@ function eventListening() {
 		$(".opendialog_header").css("cursor","move");
 		actived = $(this).parent().attr("id");
 	})
+}
+
+function imgType(id){
+	$("#"+id+" img").load(function() {
+		var h = $(this).css("height");
+		var w = $(this).css("width");
+		$(this).css("width", w);
+		calculatePosition(id, h.replace("px","")*1, w.replace("px","")*1);
+	});
+}
+
+function calculatePosition(id, h, w) {
+	//calc postion and resize
+	var height = document.documentElement.clientHeight;
+	var width = document.documentElement.clientWidth;
+	var offsetTop = (height - h - 10) / 2;
+	var offsetLeft = (width - w) / 2;
+	$("#"+id).css("top",offsetTop);
+	$("#"+id).css("left",offsetLeft);
+	$("#"+id).css("height",h);
+	$("#"+id).css("width",w);
 }
 
 function initDialog() {
